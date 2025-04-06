3a32cee1254c937d355c9412cda5399cb6c64db2
Kane Kriz <kanemkriz@gmail.com>
2025-04-05 20:48:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+// Kane Kriz
+// UWYO COSC 3020
+// Detecting Isomorphism Exercises
+// 5 April 2025
+//
+
+
+//Note: algorithm needs to handle both the case where the two graphs are isomorphic and where they are not isomorphic.
+// avoid repeated work as noted within the directions
+
+
+are_isomorphic(graph1, graph2) 
+{
+    if(//)
+        {
+    //simple check for if they are of notable different size, if sizes are not equal, immediately return false
+        }
+   
+    else(//actually begin checking, as the obvious size differences that would make isomorphism impossible are no longer a concern)
+        {
+        //case where they are isomorphic
+        return true;
+        }
+    
+        {
+        //case where they are not isomorphic
+        return false;
+        }
 }
